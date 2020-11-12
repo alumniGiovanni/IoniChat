3f3e22a6cac453256ee2c9527c21d26a6c96c3f1
Giovanni Andrade <giovanni.andrade9991@gmail.com>
2020-11-12 15:52:16 -0300

    Chat service w TODO, initial login page

--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,9 +1,51 @@
-import { Injectable } from '@angular/core';
 
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
+import * as firebase from 'firebase/app';
+import { switchMap, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+ 
+export interface User {
+  uid: string;
+  email: string;
+}
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class ChatService {
-
-  constructor() { }
+  currentUser: User = null;
+ 
+  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore) {
+    this.afAuth.onAuthStateChanged((user) => {
+      this.currentUser = user;      
+    });
+  }
+ 
+  async signup({ email, password }): Promise<any> {
+    const credential = await this.afAuth.createUserWithEmailAndPassword(
+      email,
+      password
+    );
+ 
+    const uid = credential.user.uid;
+ 
+    return this.afs.doc(
+      `users/${uid}`
+    ).set({
+      uid,
+      email: credential.user.email,
+    })
+  }
+ 
+  signIn({ email, password }) {
+    return this.afAuth.signInWithEmailAndPassword(email, password);
+  }
+ 
+  signOut(): Promise<void> {
+    return this.afAuth.signOut();
+  }
+ 
+  // TODO Chat functionality
 }
